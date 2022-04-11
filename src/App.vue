2456c1945d41ba733ca86b5c3ce91f73f67c69dd
Marium Shah <marium_shah@outlook.com>
2022-04-11 10:43:24 +0500

    Fixing inital issues

--- src/App.vue
+++ src/App.vue
@@ -14,26 +14,6 @@ export default {
   data(){
     return {
       todos:[
-        {
-          id:1,
-          title:"todo 1",
-          completed:false
-        },
-        {
-          id:2,
-          title:"todo 2",
-          completed:true
-        },
-        {
-          id:3,
-          title:"todo 3",
-          completed:false
-        },
-        {
-          id:4,
-          title:"todo 4",
-          completed:true
-        }
       ]
     }
   },
